58125beb44258c330b809268752767a05ad74c8c
avk100 <andy.karolin@gmail.com>
2017-06-08 10:37:16 -0400

    Initial commit

--- LyricSearch/SearchTunesTableViewController.h
+++ LyricSearch/SearchTunesTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  SearchTunesTableViewController.h
 //  LyricSearch
 //
 //  Created by Andy Karolin on 6/8/17.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface SearchTunesTableViewController : UITableViewController
 
 @end
-
